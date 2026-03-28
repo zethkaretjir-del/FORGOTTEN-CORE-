7387f52f41f169bb892e6b9662bc775d79cd6fcd
zethkaretjir-del <zethkaretjir@gmail.com>
2026-03-29 03:45:13 +0700

    Add Forgotten Core main files

--- install_anonymity.sh
+++ install_anonymity.sh
@@ -1,1 +1,22 @@
-404: Not Found
+#!/bin/bash
+# install_anonymity.sh - Void Anonymity Module Installer
+
+echo "[*] Installing Void Anonymity Module..."
+
+# Install anonymity tools
+if command -v pkg &> /dev/null; then
+    pkg install -y tor proxychains-ng macchanger
+else
+    apt install -y tor proxychains-ng macchanger
+fi
+
+# Install Python dependencies
+pip3 install requests[socks] pysocks stem
+pip3 install pycryptodome dnspython
+
+# Download proxy lists
+mkdir -p ~/.forgotten_core/proxies
+curl -s "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks5&timeout=10000&country=all" > ~/.forgotten_core/proxies/socks5.txt
+curl -s "https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks5.txt" >> ~/.forgotten_core/proxies/socks5.txt
+
+echo "[+] Void Anonymity Module installed!"
